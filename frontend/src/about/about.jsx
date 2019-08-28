a2eb500dd2f55f97088b8c19c24e8d726c58ca01
Sergio Reis <sergiolinkz@gmail.com>
2019-08-28 09:30:15 -0300

    Atualização dos componentes de template

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default (props) => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa historia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+        <h2>Missao e valor</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
     </div>
 )
